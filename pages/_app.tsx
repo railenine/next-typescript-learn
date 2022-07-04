31f20624f1fef45f8ca9c1920149edf2d01d4ac9
railenine <railenine@gmail.com>
2022-07-04 23:48:27 +0300

    customize document with class MyDocument

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import Head from '../node_modules/next/head';
+
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>MyTop - наш лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
