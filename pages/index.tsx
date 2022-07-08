a2d1dd41a21ef23f00c81d5d05b75adab33edbdf
railenine <railenine@gmail.com>
2022-07-08 05:10:52 +0300

    grid complete/ssr complete!!!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import { GetStaticProps } from 'next';
 import { useState } from 'react';
+import axios from 'axios';
 
 import { Button, Htag, P, Rating, Tag } from '../components';
 import { withLayout } from '../layout/Layout';
+import { MenuItem } from '../interfaces/menu.interface';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   
   const [rating, setRating] = useState<number>(4);
   
@@ -22,8 +25,32 @@ function Home(): JSX.Element {
       <Tag size='m' color='green'>green</Tag>
       <Tag size='s' color='grey'>grey</Tag>
       <Rating rating={rating} isEditable setRating={setRating}/>
+      <ul>
+        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  
+  const firstCategory = 0;
+
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PIBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  }
+}
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
