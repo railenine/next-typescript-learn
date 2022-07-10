4bbb952a173ca1df9a244b50b4d308c1175f1f33
railenine <railenine@gmail.com>
2022-07-10 21:44:00 +0300

    context

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,9 +25,6 @@ function Home({ menu }: HomeProps): JSX.Element {
       <Tag size='m' color='green'>green</Tag>
       <Tag size='s' color='grey'>grey</Tag>
       <Rating rating={rating} isEditable setRating={setRating}/>
-      <ul>
-        {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
-      </ul>
     </>
   );
 }
