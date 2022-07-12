6f964b9a4862e281ebf128d86d81463268a2eb97
railenine <railenine@gmail.com>
2022-07-12 23:11:39 +0300

    first half ui

---
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -0,0 +1,27 @@
+import cn from 'classnames';
+import { Card, HhData, Htag, Tag } from '../../components';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+
+import styles from './TopPageComponent.module.css';
+import { TopPageComponentProps } from './TopPageComponent.props';
+
+export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
+    
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.title}>
+                <Htag tag='h1'>{page.title}</Htag>
+                {products && <Tag color='grey' size='m'>{products.length}</Tag>}
+                <span>Sort</span>
+            </div>
+            <div>
+                {products && products.map(p => (<div key={p._id}>{p.title}</div>))}
+            </div>
+            <div className={styles.hhTitle}>
+                <Htag tag='h2'>Вакансии - {page.category}</Htag>
+                <Tag color='red' size='m'>hh.ru</Tag>
+            </div>
+            {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh}/>}
+        </div>
+    );
+};
