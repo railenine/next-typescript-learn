16c2672f8371c67735807a8a5ad4f5abdba259c8
railenine <railenine@gmail.com>
2022-07-13 22:11:43 +0300

    ui commit

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-import { Card, HhData, Htag, Tag } from '../../components';
+import { Advantages, HhData, Htag, P, Tag } from '../../components';
 import { TopLevelCategory } from '../../interfaces/page.interface';
 
 import styles from './TopPageComponent.module.css';
@@ -21,7 +21,14 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
                 <Htag tag='h2'>Вакансии - {page.category}</Htag>
                 <Tag color='red' size='m'>hh.ru</Tag>
             </div>
-            {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh}/>}
+            {firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh}/>}
+            {page.advantages && page.advantages.length > 0 && <>
+                <Htag tag='h2'>Преимущества</Htag>
+                <Advantages advantages={page.advantages} />
+            </>}
+            {page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{ __html: page.seoText}} />}
+            <Htag tag='h2'>Получаемые навыки</Htag>
+            {page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
         </div>
     );
 };
